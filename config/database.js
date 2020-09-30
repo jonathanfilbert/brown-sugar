e28a1f582fd85726bb99b7e0cf3995a7ae08e7cb
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-09-30 13:11:08 +0700

    feat: initialized db and add mock content

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 27017),
+        port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", ""),
         password: env("DATABASE_PASSWORD", ""),
@@ -17,3 +17,27 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+if (process.env.DATABASE_URL) {
+  const parse = require("pg-connection-string").parse;
+  const config = parse(process.env.DATABASE_URL);
+  module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+        },
+        options: {
+          ssl: false,
+        },
+      },
+    },
+  });
+}
